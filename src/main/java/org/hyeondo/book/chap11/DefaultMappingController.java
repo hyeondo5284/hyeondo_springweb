96b40a849c05e4e1f19e3becaf7062683bb064f6
hyeondo5284 <hyeondo5284@daum.net>
2019-05-10 11:39:47 +0900

    commit

---
+++ src/main/java/org/hyeondo/book/chap11/DefaultMappingController.java
@@ -0,0 +1,20 @@
+package org.hyeondo.book.chap11;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultMappingController {
+	
+	Logger logger = LogManager.getLogger();
+	
+	/**
+	 * 다른 컴트롤러에서 매핑되지 않은 URL은 이 메서드를 실행 ex)main, step1
+	 */
+	@RequestMapping("/**")
+	public void mapDefault() {
+		logger.debug("Default mapping.");
+	}
+}
